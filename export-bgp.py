44f85e333719933c966c67d8dcb0ca3c15810f59
Laurent Dumont <laurentfdumont@gmail.com>
2022-02-27 19:11:41 -0500

    Change metrics descriptions

--- export-bgp.py
+++ export-bgp.py
@@ -15,15 +15,15 @@ for key, value in json_data.items():
     for key, value in value.items():
         if key == 'remoteAs':
             remote_as = value
-            metric = Gauge('bgp_exporter_remote_as', 'Last time a batch job successfully finished', ['instance', 'neighbor_ip'], registry=registry)
+            metric = Gauge('bgp_exporter_remote_as', 'Remote AS of the BGP session', ['instance', 'neighbor_ip'], registry=registry)
             metric.labels(instance='pfsense-frr-2', neighbor_ip=neighbor_ip).set(remote_as)
         if key == 'localAs':
             local_as = value
-            metric = Gauge('bgp_exporter_local_as', 'Last time a batch job successfully finished', ['instance', 'neighbor_ip'], registry=registry)
+            metric = Gauge('bgp_exporter_local_as', 'Local AS of the BGP session', ['instance', 'neighbor_ip'], registry=registry)
             metric.labels(instance='pfsense-frr-2', neighbor_ip=neighbor_ip).set(local_as)
         if key == 'nbrDesc':
             neighbor_description = value
-            metric = Gauge('bgp_exporter_neighbor_description', 'Last time a batch job successfully finished', ['bgp_host', 'instance', 'neighbor_ip'], registry=registry)
+            metric = Gauge('bgp_exporter_neighbor_description', 'Description of the BGP session', ['bgp_host', 'instance', 'neighbor_ip'], registry=registry)
             metric.labels(bgp_host=neighbor_description, instance='pfsense-frr-2', neighbor_ip=neighbor_ip).set(0)
         if key == 'bgpState':
             bgp_state = value
@@ -31,11 +31,11 @@ for key, value in json_data.items():
                 bgp_state = 1
             else:
                 bgp_state = 666
-            metric = Gauge('bgp_exporter_neighbor_state', 'Last time a batch job successfully finished', ['instance', 'neighbor_ip'], registry=registry)
+            metric = Gauge('bgp_exporter_neighbor_state', 'State of the BGP session', ['instance', 'neighbor_ip'], registry=registry)
             metric.labels(instance='pfsense-frr-2', neighbor_ip=neighbor_ip).set(bgp_state)
         if key == 'bgpTimerUpEstablishedEpoch':
             bgp_state_established_epoch = value
-            metric = Gauge('bgp_exporter_neighbor_timestamp', 'Last time a batch job successfully finished', ['instance', 'neighbor_ip'], registry=registry)
+            metric = Gauge('bgp_exporter_neighbor_timestamp', 'Establised timestap of the BGP session', ['instance', 'neighbor_ip'], registry=registry)
             metric.labels(instance='pfsense-frr-2', neighbor_ip=neighbor_ip).set(bgp_state_established_epoch)
     print(remote_as, local_as, neighbor_description, bgp_state, bgp_state_established_epoch)
     push_to_gateway('10.10.89.10:9091', job=neighbor_ip, registry=registry)
